36a6e54a89ce96e55dc019e317576a20cfa5def2
Fontaine <tom_1993@hotmail.fr>
2016-01-30 12:59:32 +0100

    - Adding player movement + input manager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	public static InputManager instance;
+	
+	void Awake() {
+		if(instance == null) {
+			instance = this;
+		} else {
+			Destroy(this.gameObject);
+		}
+	}
+
+	public bool GoRight() {
+		if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+			return true;
+		}
+		return false;
+	}
+
+	public bool GoLeft() {
+		if(Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+			return true;
+		}
+		return false;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Player : MonoBehaviour {
+
+	public Vector2 speed;
+
+	void Update () {
+		if(InputManager.instance.GoRight()) {
+			if(speed.x < 0) {
+				speed.x *= -1;
+			}
+			transform.Translate(speed);
+		}
+		if(InputManager.instance.GoLeft()) {
+			
+			transform.Translate(-speed);
+		}
+	}
+}
