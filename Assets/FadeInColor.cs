4c5f80752818f8b56fe19d673e2a2408177b2176
Fontaine <tom_1993@hotmail.fr>
2016-01-31 00:42:48 +0100

    c le push de la vi Color + collider perso

---
+++ Assets/FadeInColor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInColor : MonoBehaviour {
+
+	public Color color1;
+	public Color color2;
+
+	public float duration = 5.0f;
+
+	private float _t;
+
+	private Light _light;
+
+	private void Awake() {
+		_light = GetComponent<Light>();
+	}
+
+	void Update () {
+		_light.color =	Color.Lerp(color1, color2, _t);
+
+		if(_t < 1) {
+			_t += Time.deltaTime / duration;
+		}
+	}
+}
