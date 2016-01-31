d672d220a181951cb7725d3ce19379d2e8595c4f
Guillaume Laruelle <g.laruelle@student.isartdigital.com>
2016-01-31 10:48:52 +0100

    Diverse things

---
+++ Assets/alarmSchool.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class alarmSchool : MonoBehaviour {
+
+    private bool _first;
+
+	// Use this for initialization
+	void Start () {
+        _first = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (Application.loadedLevel == 6)
+            {
+                if (Time.timeSinceLevelLoad == 20)
+                {
+                    if (_first == false)
+                    {
+                        this.gameObject.GetComponent<AudioSource>().Play();
+                    }
+                }
+            }
+            else if (_first == false)
+            {
+                _first = true;
+                this.gameObject.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
